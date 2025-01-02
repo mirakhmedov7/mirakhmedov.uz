628653901c3998d7a664bbad923631b469a1f2f0
Asliddin <asliddinmirakhmedov2009@gmail.com>
2025-01-02 23:41:34 +0500

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: 'Asliddin Mirakhmedov',
-  description: 'Asliddin Mirakhmedov is a software engineer who builds accessible, inclusive products and digital experiences for the web.',
+  description:
+    'Asliddin Mirakhmedov is a software engineer dedicated to creating accessible and inclusive digital experiences and web-based products.',
 }
 
 export default function RootLayout({
@@ -19,9 +20,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href='./porticon.png' />
+        <link rel="icon" href="./porticon.png" />
+        <link rel="canonical" href="https://mirakhmedov.uz/" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Person',
+              name: 'Asliddin',
+              jobTitle: 'Frontend Developer',
+              url: 'https://mirakhmedov.uz',
+              sameAs: [
+                'https://linkedin.com/in/mirakhmedov7
+                // ',
+                'https://github.com/mirakhmedov7',
+              ],
+            }),
+          }}
+        />
       </head>
-      <body className={`${inter.className} antialiased bg-slate-900 mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0 scroll-smooth`}>{children}</body>
+      <body
+        className={`${inter.className} antialiased bg-slate-900 mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0 scroll-smooth`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://mirakhmedov.uz',
+  generateRobotsTxt: true,
+};
